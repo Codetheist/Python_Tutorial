a11da16a3990e830553ee836dd8583d9b1e2cb18
Codetheist <codetheist@codetheist.com>
2021-01-14 18:57:57 -0500

    Created folders class weeks w/ subfolders for Examples and Assignment.

---
+++ COP2030_Introduction_To_Python/Week2/Assignments/main.py
@@ -0,0 +1,7 @@
+# ****************************************************************************************************
+# Assignment #1
+# Program By: Antoine Gustave
+# File Name: H1_PizzaShop.java
+# Function: This program will calculate the number of slices based on a given pizza
+# diameter and slice area.
+# ******************************************************************************************************
---
+++ COP2030_Introduction_To_Python/Week2/Examples/main.py
@@ -0,0 +1,12 @@
+# Convert feet to inches
+
+feet = float(input("Enter feet: "))
+inches = 12 * feet
+print(f"Inches is {inches}")
+
+# Convert gallons to liters
+# 1 gallon is 3.78 liters
+
+gallon = float(input("Enter gallon: "))
+liters = 3.78 * gallon
+print(f"Liters is {liters}")
