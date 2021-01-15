0ef885b4ab5fdb3df6676501096bfd9e0ea9de6e
Codetheist <codetheist@codetheist.com>
2021-01-14 19:16:37 -0500

    Modify the file to find the volume of a rectangular prism.

--- COP2030_Introduction_To_Python/Week2/Assignments/main.py
+++ COP2030_Introduction_To_Python/Week2/Assignments/main.py
@@ -1,7 +1,18 @@
 # ****************************************************************************************************
 # Assignment #1
 # Program By: Antoine Gustave
-# File Name: H1_PizzaShop.java
-# Function: This program will calculate the number of slices based on a given pizza
-# diameter and slice area.
+# File Name: main.py
+# Function: This program will calculate the volume of a rectangular prism.
 # ******************************************************************************************************
+
+# volume = length * width * height
+
+length = float(input("Enter length: "))
+
+width = float(input("Enter width: "))
+
+height = float(input("Enter height: "))
+
+area = length * width * height
+
+print(f"The area of the rectangle is {area} in.")
