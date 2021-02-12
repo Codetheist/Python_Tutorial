8c05b797c46792d74bf979b0eff6123bfdcf9cde
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-11 19:08:09 -0500

    Learning how to use file handling and dictionaries

--- COP2030_Introduction_To_Python/Week4/Examples/main.py
+++ COP2030_Introduction_To_Python/Week4/Examples/main.py
@@ -1,1 +1,26 @@
-ECHO is on.
+# grades = [82.4, 90.1, 75.4, 79.9]
+
+# total = 0.0
+# for grade in grades:
+#     total += grade
+#     avg = total / len(grades)
+#     print(avg)
+
+# def max_val(numbers):
+    #grades = [82.4, 90.1, 75.4, 79.9]
+
+#     largest = numbers[0]
+
+#     for number in numbers:
+#         if number > largest:
+#             largest = grade
+#     return largest
+
+# grades = [82.4, 90.1, 75.4, 79.9]
+# max_val(grades)
+
+# learning how to use file handling.
+with open("data.csv") as f:
+    for line in f:
+        line = line.rstrip()
+        print(line)
