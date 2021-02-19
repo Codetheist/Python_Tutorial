2cace837274b17923391b5ede7cfc971fc8fb14d
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-18 19:18:41 -0500

    Initializing homework

---
+++ COP2030_Introduction_To_Python/Week6/Assignments/dictionary.py
@@ -0,0 +1,35 @@
+# ****************************************************************************************************
+# Assignment #5
+# Program By: Antoine Gustave
+# File Name: dictionary.py
+# Function:
+# students = {
+#    'John': {
+#        'email': 'john@irsc.edu',
+#        'phone': '555-1234',
+#        'address': '123 irsc st'
+#    },
+
+#    'Mary': {
+#        'email': 'mary@irsc.edu',
+#        'phone': '555-1235',
+#        'address': '500 irsc st'
+#    },
+
+#    'Chris': {
+#        'email': 'chris@irsc.edu',
+#        'phone': '555-1236',
+#        'address': '900 irsc st'
+#    },
+
+#    'Alison': {
+#        'email': 'alison@irsc.edu',
+#        'phone': '555-1237',
+#        'address': '1205 irsc st'
+#    }
+# }
+# 1. Print all the students names using a loop
+# 2. Print all the email addresses using a loop
+# 3. Create a new student named 'Bob' and assign him a fake email, phone, and address. This should be exactly four lines of code.
+# 4. In a single line of code, print Mary's phone
+# ******************************************************************************************************
