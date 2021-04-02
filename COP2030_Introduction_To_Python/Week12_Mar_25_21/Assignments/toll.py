d4a9f1b959403c25a9481260e6544bc8a1c50377
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-04-01 21:53:58 -0400

    Change
    No new change other than saving the file.

--- COP2030_Introduction_To_Python/Week12_Mar_25_21/Assignments/toll.py
+++ COP2030_Introduction_To_Python/Week12_Mar_25_21/Assignments/toll.py
@@ -1,24 +1,36 @@
 # ****************************************************************************************************
-# Assignment #
+# Assignment #6
 # Program By: Antoine Gustave
 # File Name: toll.py
 # Function:Assume today's date is 100.
-# For this toll, a car is $1.00 and truck is $1.50.  As each vehicle passes through the toll, keep a sum total of the total funds collected as well as total number of vehicles.. 
-# If a license plate is expired, an exception should be raised so the vehicle can be flagged (just raise the exception, don't worry about handling it)
+# For this toll, a car is $1.00 and truck is $1.50.  As each vehicle passes through the toll,
+# keep a sum total of the total funds collected as well as total number of vehicles..
+# If a license plate is expired, an exception should be raised so the vehicle
+# can be flagged (just raise the exception, don't worry about handling it)
 # ******************************************************************************************************
 
+# Create a TollBooth class.
 
+# Use the following dictionary to represent all the vehicles
 vehicles = {
     'ABC746': {
-        'type':'car',
+        'type': 'car',
         'expires': 105
     },
     'ZZA398': {
-        'type':'truck',
+        'type': 'truck',
         'expires': 110
     },
     'BFH918': {
-        'type':'car',
+        'type': 'car',
         'expires': 90
     },
 }
+
+# The class will have a function/method called
+
+# lpn is short for license plate number
+
+
+def charge_toll(self, lpn):
+    name = 0
--- COP2030_Introduction_To_Python/Week12_Mar_25_21/Examples/main.py
+++ COP2030_Introduction_To_Python/Week12_Mar_25_21/Examples/main.py
@@ -1,1 +1,24 @@
-ECHO is on.
+# Separating out normal correct running code from our error handling code
+def weeks_to_days(weeks):
+    if weeks > 0:
+        days = weeks * 7
+        return days
+    else:
+        raise ValueError("Weeks must be postitive")
+
+
+try:
+    days = weeks_to_days(2)
+    print(days)
+except ValueError as e:
+    print("An error happened")
+    print(e)
+
+
+class Car:
+    def __init__(self):
+        print('Constructor being called')
+        self.current_gear = 0
+
+
+mycar = Car()
